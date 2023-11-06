e13a9f559758823e5f86947ec2ff804423530ac6
Sebastian Castro <scastro@qvision.com.co>
2023-11-05 22:37:45 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'KEEP LEARNING, KEEP MOVING AHEAD'
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
